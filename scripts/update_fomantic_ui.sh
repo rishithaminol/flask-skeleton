28b85fac0e9edfa7f15ec37adc369c5fc288fcd8
Rishitha Minol <rishithaminol@gmail.com>
2020-01-16 12:57:13 +0530

    change .env file and added new functionality to the scripts

--- scripts/update_fomantic_ui.sh
+++ scripts/update_fomantic_ui.sh
@@ -4,3 +4,36 @@
 # npm install
 # Edit semantic.json file to ignore .css files and only keep min.css files
 # Copy dist directory as assets/fomantic-ui
+
+
+
+git clone https://github.com/fomantic/Fomantic-UI.git
+cd Fomantic-UI
+
+cat > semantic.json <<EOF
+{
+  "base": "",
+  "paths": {
+    "source": {
+      "config": "src/theme.config",
+      "definitions": "src/definitions/",
+      "site": "src/site/",
+      "themes": "src/themes/"
+    },
+    "output": {
+      "packaged": "dist/",
+      "uncompressed": "dist2/components/",
+      "compressed": "dist/components/",
+      "themes": "dist/themes/"
+    },
+    "clean": "dist/"
+  },
+  "permission": false,
+  "autoInstall": false,
+  "rtl": false,
+  "version": "2.8.3"
+}
+EOF
+
+rm -rf 'node_modules'
+npm install
