08323821daca5df4d70549f7b478b84a0ee5d4b3
Rishitha Minol <rishithaminol@gmail.com>
2020-05-10 14:08:58 +0530

    Main user login error handling user experience

--- src/routes/user_login.py
+++ src/routes/user_login.py
@@ -8,18 +8,34 @@ user_login = Blueprint('user_login', __name__, url_prefix='/user')
 
 @user_login.route('/login', methods=['POST', 'GET'], strict_slashes=False)
 def sign_in():
+    data = {
+        'message': "",
+        'login_failure': False,
+        'response_code': 200
+    }
+
     if request.method == 'POST':
         username = request.form.get('user_name')
         password = request.form.get('password')
 
+        if username is None or password is None:
+            data['message'] = "Empty fields not allowed"
+            data['login_failure'] = True
+            data['response_code'] = 401
+            return render_template('login.html', data=data), data['response_code']
+
         if check_user_credentials(username, gen_user_pass_hash(password)):
             user_data = User(username)
             login_user(user=user_data)
+        else:
+            data['message'] = "Username or email does not exists"
+            data['login_failure'] = True
+            data['response_code'] = 401
 
     if current_user.is_authenticated:
         return redirect('/')
 
-    return render_template('login.html')
+    return render_template('login.html', data=data), data['response_code']
 
 @user_login.route('/logout', methods=['GET'], strict_slashes=False)
 def sign_out():
